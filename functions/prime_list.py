a2e1883a46c47f4b736da6136621b1a91743630a
Japhet09 <japhetalfred19@gmail.com>
2022-07-27 23:30:40 +0200

    Deleted is_prime() and imported it as a module instead

--- functions/prime_list.py
+++ functions/prime_list.py
@@ -1,22 +1,8 @@
 from is_prime import *
 
 def main():
-    n = int(input("Number: "))
-    if is_prime(n):
-        print("the number is prime")
-    else:
-       print("The number is not prime")
-
     range_of_nums = range_list()
     prime_list(range_of_nums)
-
-def is_prime(num):
-    prime = True
-    for i in range(2,num):
-        if num%i ==0:
-            prime = False
-            break
-    return prime
 
 
 def range_list():
