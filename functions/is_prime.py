196cbc5aa5f7f33d21a4d748f13ae7369982de02
Japhet09 <japhetalfred19@gmail.com>
2022-07-27 13:27:50 +0200

    wrote the function to check if number is prime

---
+++ functions/is_prime.py
@@ -0,0 +1,17 @@
+def main():
+    n = int(input("Number: "))
+    if is_prime(n):
+        print("the number is prime")
+    else:
+       print("The number is not prime")
+      
+    
+def is_prime(num):
+    prime = True
+    for i in range(2,num):
+        if num%i ==0:
+            prime = False
+            break
+    return prime
+
+main()
