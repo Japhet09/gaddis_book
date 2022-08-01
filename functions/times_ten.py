38d90186575dcbced7827e72d03a7ac5b78b7efb
Japhet09 <japhetalfred19@gmail.com>
2022-08-01 14:10:25 +0200

    number times ten

---
+++ functions/times_ten.py
@@ -0,0 +1,13 @@
+def main():
+    n = int(input("Enter number "))
+    times_ten(n)
+
+def times_ten(num):
+    """
+      The function accept user number and output the 
+    number times 10
+    ”””
+    """
+    print(f"The product of {num} * 10 is {num * 10} ") 
+
+main()
