4934c378c93a43b31fe95ef445f7a114b07c0ac9
Japhet09 <japhetalfred19@gmail.com>
2022-08-01 21:50:13 +0200

    added option for user to continue playing

--- functions/guessing_game.py
+++ functions/guessing_game.py
@@ -1,15 +1,24 @@
+from curses import A_CHARTEXT
 import random
 def main():
-    num = random.randint(1,100)
-    guess(num)
-        
+    guess()
+    
+    while True:
+        again = input("Enter Y to play again: ")
+        if again.lower() == "y":
+            guess()
+        else:
+            break
+            
 
-def guess(n):
+def guess():
     """
     User guess what the number is, display message 
     if the   guess is higher or lower
     """
+    n = random.randint(1,100)
     counter = 0
+    
     while True:
         user = int(input("Enter a number: "))
         if user > n:
